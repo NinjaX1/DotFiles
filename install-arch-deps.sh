81c7d14725bcc7eb6a4cabfe91abe7cd0ee4120b
NinjaX1 <joao.gabrielvs@hotmail.com>
2022-09-13 14:16:37 -0300

    Update packages needed

--- install-arch-deps.sh
+++ install-arch-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S xclip maim xorg xorg-server picom feh dunst acpi pcmanfm firefox lxappearance neofetch xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting --noconfirm --needed
+sudo pacman -S xorg xorg-server xorg-xinit libx11 libxinerama libx11 webkit2gtk xclip maim picom feh dunst acpi pcmanfm lxappearance neofetch xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting --noconfirm --needed
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.p10k
